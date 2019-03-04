45d5c07479c19cfe5782466d13fa8e0a85cfd00a
Jason Dufair <jase@dufair.org>
2019-03-04 14:34:00 -0500

    Make box function

--- dashboard.el
+++ dashboard.el
@@ -21,25 +21,12 @@
 
 ;;; Code:
 
-(require 'bookmark)
-(require 'calendar)
-(require 'page-break-lines)
-(require 'recentf)
-(require 'register)
-
 (require 'dashboard-widgets)
 
 ;; Custom splash screen
 (defvar dashboard-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map [tab] 'widget-forward)
-    (define-key map (kbd "C-i") 'widget-forward)
-    (define-key map [backtab] 'widget-backward)
-    (define-key map (kbd "RET") 'widget-button-press)
-    (define-key map [down-mouse-1] 'widget-button-click)
     (define-key map (kbd "g") #'dashboard-refresh-buffer)
-    (define-key map (kbd "}") #'dashboard-next-section)
-    (define-key map (kbd "{") #'dashboard-previous-section)
     map)
   "Keymap for dashboard mode.")
 
@@ -52,7 +39,7 @@
   :abbrev-table nil
   (whitespace-mode -1)
   (linum-mode -1)
-  (page-break-lines-mode 1)
+  (overwrite-mode)
   (setq inhibit-startup-screen t)
   (setq buffer-read-only t
         truncate-lines t))
@@ -69,105 +56,87 @@
 (defconst dashboard-buffer-name "*dashboard*"
   "Dashboard's buffer name.")
 
-(defvar dashboard--section-starts nil
-  "List of section starting positions.")
-
-(defun dashboard-previous-section ()
-  "Navigate back to previous section."
-  (interactive)
-  (let ((current-section-start nil)
-        (current-position (point))
-        (previous-section-start nil))
-    (dolist (elt dashboard--section-starts)
-      (when (and current-section-start
-                 (not previous-section-start))
-        (setq previous-section-start elt))
-      (when (and (not current-section-start)
-                 (< elt current-position))
-        (setq current-section-start elt)))
-    (goto-char (if (eq current-position current-section-start)
-                   previous-section-start
-                 current-section-start))))
-
-(defun dashboard-next-section ()
-  "Navigate forward to next section."
-  (interactive
-   (let ((current-position (point))
-         (next-section-start nil)
-         (section-starts (reverse dashboard--section-starts)))
-     (dolist (elt section-starts)
-       (when (and (not next-section-start)
-                  (> elt current-position))
-         (setq next-section-start elt)))
-     (when next-section-start
-       (goto-char next-section-start)))))
-
-(defun dashboard-maximum-section-length ()
-  "For the just-inserted section, calculate the length of the longest line."
-  (let ((max-line-length 0))
-    (save-excursion
-      (dashboard-previous-section)
-      (while (not (eobp))
-        (setq max-line-length
-              (max max-line-length
-                   (- (line-end-position) (line-beginning-position))))
-        (forward-line)))
-    max-line-length))
+(defun draw-box (title top left width height)
+  "Draw a box with TITLE starting at TOP LEFT with dimensions WIDTH x HEIGHT (in characters)."
+  (goto-char (point-min))
+  (forward-line top)
+  (forward-char left)
+  (insert "┌")
+  (let* ((half (/ (- width 4 (length title)) 2.0))
+        (l (floor half))
+        (r (ceiling half)))
+    (insert (make-string l ?─))
+    (insert (propertize (format " %s " title) 'face 'dashboard-heading))
+    (insert (make-string r ?─))
+    (insert "┐")
+    (newline))
+  (dotimes (i (- height 2))
+    (insert "│")
+    (insert (make-string (- width 2) ?\ ))
+    (insert "│")
+    (newline))
+  (insert "└")
+  (insert (make-string (- width 2) ?─))
+  (insert "┘"))
 
 (defun dashboard-insert-startupify-lists ()
   "Insert the list of widgets into the buffer."
   (interactive)
-  (let ((buffer-exists (buffer-live-p (get-buffer dashboard-buffer-name)))
-        (save-line nil)
-        (recentf-is-on (recentf-enabled-p))
-        (origial-recentf-list recentf-list)
-        (dashboard-num-recents (or (cdr (assoc 'recents dashboard-items)) 0))
-        (max-line-length 0))
-    ;; disable recentf mode,
-    ;; so we don't flood the recent files list with org mode files
-    ;; do this by making a copy of the part of the list we'll use
-    ;; let dashboard widgets change that
-    ;; then restore the orginal list afterwards
-    ;; (this avoids many saves/loads that would result from
-    ;; disabling/enabling recentf-mode)
-    (if recentf-is-on
-        (setq recentf-list (seq-take recentf-list dashboard-num-recents))
-      )
-    (when (or (not (eq dashboard-buffer-last-width (window-width)))
-              (not buffer-exists))
-      (setq dashboard-banner-length (window-width)
-            dashboard-buffer-last-width dashboard-banner-length)
-      (with-current-buffer (get-buffer-create dashboard-buffer-name)
-        (let ((buffer-read-only nil)
-              (list-separator "\n\n"))
-          (erase-buffer)
-          (dashboard-insert-banner)
-          (dashboard-insert-page-break)
-          (setq dashboard--section-starts nil)
-          (mapc (lambda (els)
-                  (let* ((el (or (car-safe els) els))
-                         (list-size
-                          (or (cdr-safe els)
-                              dashboard-items-default-length))
-                         (item-generator
-                          (cdr-safe (assoc el dashboard-item-generators))))
-                    (add-to-list 'dashboard--section-starts (point))
-                    (funcall item-generator list-size)
-                    (setq max-line-length
-                          (max max-line-length (dashboard-maximum-section-length)))
-                    (dashboard-insert-page-break)))
-                dashboard-items)
-          (when dashboard-center-content
-            (goto-char (car (last dashboard--section-starts)))
-            (let ((margin (floor (/ (max (- (window-width) max-line-length) 0)  2))))
-              (while (not (eobp))
-                (and (not (eq ? (char-after)))
-                     (insert (make-string margin ?\ )))
-                (forward-line 1)))))
-        (dashboard-mode)
-        (goto-char (point-min))))
-    (if recentf-is-on
-        (setq recentf-list origial-recentf-list))))
+  ;; (let ((buffer-exists (buffer-live-p (get-buffer dashboard-buffer-name)))
+  ;;       (save-line nil)
+  ;;       (recentf-is-on (recentf-enabled-p))
+  ;;       (origial-recentf-list recentf-list)
+  ;;       (dashboard-num-recents (or (cdr (assoc 'recents dashboard-items)) 0))
+  ;;       (max-line-length 0))
+  ;;   ;; disable recentf mode,
+  ;;   ;; so we don't flood the recent files list with org mode files
+  ;;   ;; do this by making a copy of the part of the list we'll use
+  ;;   ;; let dashboard widgets change that
+  ;;   ;; then restore the orginal list afterwards
+  ;;   ;; (this avoids many saves/loads that would result from
+  ;;   ;; disabling/enabling recentf-mode)
+  ;;   (if recentf-is-on
+  ;;       (setq recentf-list (seq-take recentf-list dashboard-num-recents))
+  ;;     )
+  ;;   (when (or (not (eq dashboard-buffer-last-width (window-width)))
+  ;;             (not buffer-exists))
+  ;;     (setq dashboard-banner-length (window-width)
+  ;;           dashboard-buffer-last-width dashboard-banner-length)
+  ;;     (with-current-buffer (get-buffer-create dashboard-buffer-name)
+  ;;       (let ((buffer-read-only nil)
+  ;;             (list-separator "\n\n"))
+  ;;         (erase-buffer)
+  ;;         (dashboard-insert-banner)
+  ;;         (dashboard-insert-page-break)
+  ;;         (setq dashboard--section-starts nil)
+  ;;         (mapc (lambda (els)
+  ;;                 (let* ((el (or (car-safe els) els))
+  ;;                        (list-size
+  ;;                         (or (cdr-safe els)
+  ;;                             dashboard-items-default-length))
+  ;;                        (item-generator
+  ;;                         (cdr-safe (assoc el dashboard-item-generators))))
+  ;;                   (add-to-list 'dashboard--section-starts (point))
+  ;;                   (funcall item-generator list-size)
+  ;;                   (setq max-line-length
+  ;;                         (max max-line-length (dashboard-maximum-section-length)))
+  ;;                   (dashboard-insert-page-break)))
+  ;;               dashboard-items)
+  ;;         (when dashboard-center-content
+  ;;           (goto-char (car (last dashboard--section-starts)))
+  ;;           (let ((margin (floor (/ (max (- (window-width) max-line-length) 0)  2))))
+  ;;             (while (not (eobp))
+  ;;               (and (not (eq ? (char-after)))
+  ;;                    (insert (make-string margin ?\ )))
+  ;;               (forward-line 1)))))
+  ;;       (dashboard-mode)
+  ;;       (goto-char (point-min))))
+  ;;   (if recentf-is-on
+  ;;       (setq recentf-list origial-recentf-list)))
+  (with-current-buffer (get-buffer-create dashboard-buffer-name)
+    (dashboard-mode)
+    (let ((buffer-read-only nil))
+      (draw-box "stuff" 0 0 30 30))))
 
 (add-hook 'window-setup-hook
           (lambda ()
